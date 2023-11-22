a4fd3b61e9e721dcbcc21ce8ee2a1b95773efca6
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-22 19:03:56 +0100

    cambio

--- login.js
+++ login.js
@@ -2,9 +2,8 @@ function login() {
     const nombre = document.getElementById('username').value;
     const contrasenia = document.getElementById('password').value;
     // Obtener datos del localStorage
-    const pedido_local = localStorage.getItem('pedido');
-    const pedido = JSON.parse(pedido_local);
-    if (nombre === pedido.nombre && contrasenia === pedido.contrasenia) {
+    const usuario = JSON.parse(localStorage.getItem('usuario'));
+    if (nombre === usuario.nombre && contrasenia === usuario.contrasenia) {
         alert('Inicio de sesión exitoso');
         window.location.href = 'index.html';
         // Redirige o realiza otras acciones necesarias después del inicio de sesión
