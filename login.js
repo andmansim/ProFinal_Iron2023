47743a3d24d0ca793c3f506d4cbd50895936af40
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-22 17:12:24 +0100

    no va la wifi

--- login.js
+++ login.js
@@ -1,11 +1,15 @@
 function login() {
     const nombre = document.getElementById('username').value;
     const contrasenia = document.getElementById('password').value;
-
-    if (nombre === 'usuario' && contrasenia === 'contrasena') {
+    // Obtener datos del localStorage
+    const nombre_verificar = localStorage.getItem('nombre');
+    const contrasenia_verificar = localStorage.getItem('contrasena');
+    if (nombre === nombre_verificar && contrasenia === contrasenia_verificar) {
         alert('Inicio de sesión exitoso');
+        window.location.href = 'index.html';
         // Redirige o realiza otras acciones necesarias después del inicio de sesión
     } else {
         alert('Credenciales incorrectas. Inténtalo de nuevo.');
+        
     }
 }
--- registro.js
+++ registro.js
@@ -1,25 +1,25 @@
 function registrarUsuario() {
-    
     const nombre = document.getElementById('username').value;
     const contrasenia = document.getElementById('password').value;
     const direccion = document.getElementById('direccion').value;
     const email = document.getElementById('email').value;
-
-    
+    const telefono = document.getElementById('telefono').value;
 
     // Obtener datos del localStorage
     const nombre_verificar = localStorage.getItem('nombre');
-    
+
     // Verificando usuario
     if (nombre_verificar) {
         alert(`El nombre de usuario ${nombre} ya existe, por favor elija otro`);
     } else {
         // Almacenar datos
         localStorage.setItem('nombre', nombre);
-        localStorage.setItem('contrasena', contrasenia);
+        localStorage.setItem('contrasenia', contrasenia);
         localStorage.setItem('direccion', direccion);
         localStorage.setItem('email', email);
+        localStorage.setItem('telefono', telefono);
         alert('Se ha registrado con éxito');
+        window.location.href = 'index.html';
     }
 }
 
