0bf82e4414d2798c0199314186b75b25889b1cca
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-27 11:05:58 +0100

    ya

--- yogur_helado.js
+++ yogur_helado.js
@@ -10,45 +10,41 @@ function obtenerSeleccionados(nombre) {
 }
 
 function guardarDatos_yogurhelado() {
-    try{
-        // Obtener valores seleccionados
-        alert('Puta madre');
-        let base = document.querySelector('input[name="base"]:checked').value;
-        let toppings = obtenerSeleccionados('topping_');
-        let salsas = obtenerSeleccionados('salsa_');
-        let extras = obtenerSeleccionados('extra_');
-        alert('Puta madre');
-        let pedido = {
-            'nombre': 'yogur helado',
-            'topping': toppings,
-            'base': base,
-            'salsas': salsas, 
-            'extras': extras
-        };
     
-        let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    // Obtener valores seleccionados
+    alert('Puta madre');
+    let base = document.querySelector('input[name="base"]:checked').value;
+    let toppings = obtenerSeleccionados('topping_');
+    let salsas = obtenerSeleccionados('salsa_');
+    let extras = obtenerSeleccionados('extra_');
+    alert('Puta madre');
+    let pedido = {
+        'nombre': 'yogur helado',
+        'topping': toppings,
+        'base': base,
+        'salsas': salsas, 
+        'extras': extras
+    };
 
-        carrito.push(pedido);
-        // Almacenar en localStorage
-        localStorage.setItem('carrito', JSON.stringify(carrito));
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-        // Puedes redirigir a otra página o realizar otras acciones aquí
-        alert('Su pedido se ha añadido con éxito');
-        window.location.href = 'index.html';
-    } catch (e) {
-        alert('Error: ' + e.message);
-    }
+    carrito.push(pedido);
+    // Almacenar en localStorage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    // Puedes redirigir a otra página o realizar otras acciones aquí
+    alert('Su pedido se ha añadido con éxito');
+    window.location.href = '/index.html';
 }
 
 
 
 function guardarDatos(dato){
     const dato_guardar = document.querySelector(`input[name="${dato}"]:checked`).value;
-    alert(dato_guardar);
     let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
     if (dato_guardar == 'Combo 1'){
-    çpedido = {
+        pedido = {
             'nombre': dato_guardar, 
             'topping': 'Oreo',
             'yogur': 'Natural con frutas',
@@ -66,19 +62,18 @@ function guardarDatos(dato){
             'yogur': 'Mango con coco',
             'salsa': 'Kiwi'};
     } else {
-        alert('cosa')
         pedido = {
             'nombre': dato_guardar, 
             'tipo': dato};
             
     };
-
-    alert(JSON.stringify(pedido)); 
     
     carrito.push(pedido);
     
     localStorage.setItem('carrito', JSON.stringify(carrito));
     
     alert('Su pedido se ha añadido con éxito');
-    window.location.href = 'index.html';
+    window.location.href = '/index.html';
+    
+    //window.location.replace('/index.html');
 }
