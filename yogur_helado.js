ce60e60df29bc2afdc8a8af4e34b5fe9052c9642
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-27 10:19:59 +0100

    ddddd

--- yogur_helado.js
+++ yogur_helado.js
@@ -1,35 +1,41 @@
-function guardarDatos_yogurhelado() {
-
-    // Obtener valores seleccionados
-    const base = document.querySelector('input[name="base"]:checked').value;
-    const toppings = document.obtenerSeleccionados('topping');
-    const salsas = document.obtenerSeleccionados('salsa');
-    const extras = document.obtenerSeleccionados('extra');
-    alert('Puta madre')
-    pedido = {
-        'nombre': 'yogur helado',
-        'topping': toppings,
-        'base': base,
-        'salsas': salsas, 
-        'extras': extras
-    }
-
-    let carrito = JSON.parse(localStorage.getItem('pedido')) || [];
-
-    carrito.push(pedido);
-    // Almacenar en localStorage
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-
-    // Puedes redirigir a otra página o realizar otras acciones aquí
-    alert('Su pedido se ha añadido con éxito');
-    window.location.href = 'index.html';
-}
 
 function obtenerSeleccionados(nombre) {
     //obtener todos los checkbox seleccionados
     const checkboxes = document.querySelectorAll(`input[name="${nombre}"]:checked`);
     return Array.from(checkboxes, checkbox => checkbox.value);
 }
+
+function guardarDatos_yogurhelado() {
+    try{
+        // Obtener valores seleccionados
+        const base = document.querySelector('input[name="base"]:checked').value;
+        const toppings = document.obtenerSeleccionados('topping');
+        const salsas = document.obtenerSeleccionados('salsa');
+        const extras = document.obtenerSeleccionados('extra');
+        alert('Puta madre');
+        pedido = {
+            'nombre': 'yogur helado',
+            'topping': toppings,
+            'base': base,
+            'salsas': salsas, 
+            'extras': extras
+        };
+    
+        let carrito = JSON.parse(localStorage.getItem('pedido')) || [];
+
+        carrito.push(pedido);
+        // Almacenar en localStorage
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+
+        // Puedes redirigir a otra página o realizar otras acciones aquí
+        alert('Su pedido se ha añadido con éxito');
+        window.location.href = 'index.html';
+    } catch (e) {
+        alert('Error: ' + e.message);
+    }
+}
+
+
 
 function guardarDatos(dato){
     const dato_guardar = document.querySelector(`input[name="${dato}"]:checked`).value;
