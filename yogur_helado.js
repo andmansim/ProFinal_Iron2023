475eabb24bd18315de6129fa874642626f7b1057
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-27 10:43:29 +0100

    probamos

--- yogur_helado.js
+++ yogur_helado.js
@@ -1,19 +1,24 @@
 
 function obtenerSeleccionados(nombre) {
     //obtener todos los checkbox seleccionados
-    const checkboxes = document.querySelectorAll(`input[name="${nombre}"]:checked`);
-    return Array.from(checkboxes, checkbox => checkbox.value);
+    let ingredientes = [];
+    const checkboxes = document.querySelectorAll(`input[name^="${nombre}"]:checked`);
+    checkboxes.forEach(function (checkbox) {
+        ingredientes.push(checkbox.value);
+    });
+    return ingredientes;
 }
 
 function guardarDatos_yogurhelado() {
     try{
         // Obtener valores seleccionados
-        const base = document.querySelector('input[name="base"]:checked').value;
-        const toppings = document.obtenerSeleccionados('topping');
-        const salsas = document.obtenerSeleccionados('salsa');
-        const extras = document.obtenerSeleccionados('extra');
         alert('Puta madre');
-        pedido = {
+        let base = document.querySelector('input[name="base"]:checked').value;
+        let toppings = obtenerSeleccionados('topping_');
+        let salsas = obtenerSeleccionados('salsa_');
+        let extras = obtenerSeleccionados('extra_');
+        alert('Puta madre');
+        let pedido = {
             'nombre': 'yogur helado',
             'topping': toppings,
             'base': base,
@@ -39,10 +44,33 @@ function guardarDatos_yogurhelado() {
 
 function guardarDatos(dato){
     const dato_guardar = document.querySelector(`input[name="${dato}"]:checked`).value;
-    
+    if (dato_guardar == 'Combo 1'){
+        let pedido = {
+            'nombre': dato_guardar, 
+            'topping': 'Oreo',
+            'yogur': 'Natural con frutas',
+            'salsa': 'Fresa'};
+    } else if (dato_guardar == 'Combo 2'){
+        let pedido = {
+            'nombre': dato_guardar, 
+            'topping': 'Chocolate blanco',
+            'yogur': 'Vainilla con alemendras',
+            'salsa': 'Caramelo'};
+    } else if (dato_guardar == 'Combo 3'){
+        let pedido = {
+            'nombre': dato_guardar, 
+            'topping': 'Frutas mixtas',
+            'yogur': 'Mango con coco',
+            'salsa': 'Kiwi'};
+    } else {
+        let pedido = {
+            'nombre': dato_guardar, 
+            'tipo': dato};
+    };
+
     const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-    carrito[0].dato = dato_guardar;
-    localStorage.setItem('carrito', carrito );
+    carrito.push(pedido);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
     
     alert('Su pedido se ha añadido con éxito');
     window.location.href = 'index.html';
